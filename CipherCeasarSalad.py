6ec1b0b246343faa577d914966fee2ecb857f518
Virendra Ukey <vukey@qualys.com>
2021-04-30 16:41:19 +0530

    cesar cipher

---
+++ CipherCeasarSalad.py
@@ -0,0 +1,22 @@
+class CipherCeasarSalad:
+    def cipher(text, shiftBy):
+        result = ""
+        special_characters = '"!@#$%^&*()-+?_=,<>/"[]|\\{}" "'
+        for i in range(len(text)):
+            char = text[i]
+            if char in special_characters:
+                result += str(ord(char))
+            elif char.isnumeric():
+                result += str(ord(char))
+            elif char.isupper():
+                result += chr((ord(char) + shiftBy-65) % 26 + 65)
+            else:
+                result += chr((ord(char) + shiftBy - 97) % 26 + 97)
+        return result
+
+
+if __name__ == '__main__':
+    text = input("please enter text to entrypt...")
+    shift = int(input("shift by.."))
+    ciperText = CipherCeasarSalad.cipher(text, shift)
+    print(ciperText)
